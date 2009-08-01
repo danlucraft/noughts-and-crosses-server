0250983f14e2807334325e12b2d943b0bf3820b8
Daniel Lucraft <danlucraft@The-Oblong-Office.local>
2009-08-01 11:13:36 +0100

    Map numbers to strings with a case statement

--- lib/main.scala
+++ lib/main.scala
@@ -3,7 +3,13 @@ object NoughtsAndCrosses {
 	val board = Array(0, 1, 2, 2, 0, 1, 1, 0, 0)
 	
 	def pretty = {
-		board.map((x:Int) => x*2)
+		board.map((x:Int) => 
+		  x match {
+			case 0 => "."
+			case 1 => "o"
+			case 2 => "x"
+		}
+		)
 	}
 	
 	def main(args: Array[String]) {
