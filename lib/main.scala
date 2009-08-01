8ecb858d5078bdceef70566e010375e91cd80f40
Daniel Lucraft <danlucraft@The-Oblong-Office.local>
2009-08-01 11:15:22 +0100

    Map board numbers to strings with a Map

--- lib/main.scala
+++ lib/main.scala
@@ -1,15 +1,10 @@
 
 object NoughtsAndCrosses {
 	val board = Array(0, 1, 2, 2, 0, 1, 1, 0, 0)
+	val symbolMap = Map(0 -> ".", 1 -> "o", 2 -> "x")
 	
 	def pretty = {
-		board.map((x:Int) => 
-		  x match {
-			case 0 => "."
-			case 1 => "o"
-			case 2 => "x"
-		}
-		)
+		board.map((x:Int) => symbolMap(x))
 	}
 	
 	def main(args: Array[String]) {
