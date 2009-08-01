ce97c2b3985c7866c058e48fca33dd7eb700a32f
Daniel Lucraft <danlucraft@The-Oblong-Office.local>
2009-08-01 12:06:22 +0100

    Can give a board on the command line like scala NoughtsAndCrosses 012210000

--- lib/main.scala
+++ lib/main.scala
@@ -10,12 +10,20 @@ class Board(val size: Int) {
 	def pretty = {
 		board.map(prettyRow(_)).mkString("\n")
 	}
-}
+	
+	def set(x:Int, y:Int, v:Int) {
+		board(x)(y) = v
+	}
+}	
 
 object NoughtsAndCrosses {
 	def main(args: Array[String]) {
-		val board = new Board(args.first.toInt)
+		val board = new Board(3)
+		val ints = args.first.split("").subArray(1, 10).map(_.toInt)
+		for (i <- 0 to 2; j <- 0 to 2) 
+		  board.set(i, j, ints(i*3 + j))
 		println(board.pretty.toString)
+		
 	}
 }
 
