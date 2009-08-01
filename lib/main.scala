5ae25f29e765a0d210557674bdee2db9c47aa9ab
Daniel Lucraft <danlucraft@The-Oblong-Office.local>
2009-08-01 11:25:01 +0100

    Use array of arrays to make things easier

--- lib/main.scala
+++ lib/main.scala
@@ -1,10 +1,16 @@
 
 object NoughtsAndCrosses {
-	val board = Array(0, 1, 2, 2, 0, 1, 1, 0, 0)
+	val board = Array(Array(0, 1, 2), 
+	                  Array(2, 0, 1), 
+	                  Array(1, 0, 0))
 	val symbolMap = Map(0 -> ".", 1 -> "o", 2 -> "x")
 	
+	def prettyRow(row:Array[Int]) = {
+		row.map((x:Int) => symbolMap(x))
+	}
+	
 	def pretty = {
-		board.map((x:Int) => symbolMap(x))
+		board.map(prettyRow(_).toString)
 	}
 	
 	def main(args: Array[String]) {
