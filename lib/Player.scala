8fcfd673e2c5418695cf83136b22a07c22071380
Daniel Lucraft <danlucraft@The-Oblong-Office.local>
2009-08-13 15:33:29 +0100

    Added main methods to players for command-line usage

--- lib/Player.scala
+++ lib/Player.scala
@@ -1,4 +1,11 @@
 
 abstract class Player {
 	def move(board:Board, player:Int): Option[(Int, Int)]
+	
+	def main(args: Array[String]) {
+		println("Current board: ")
+		val board = Board.fromString(args(0))
+		println(board.pretty.toString)
+        NoughtsAndCrosses.printMove(move(board, 1))
+	}
 }
